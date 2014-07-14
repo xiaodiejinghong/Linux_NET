5eb45320bd534b4dcd6a3e5bf4ddc5de9caa3894
comply <com@01.com>
2014-07-14 18:49:49 +0800

    Linux.NET 学习手记（8） Demo1 提交

---
+++ Linux_NET_8/Demo1/MyMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+namespace Demo1
+{
+    using AppFun = Func<IDictionary<string, object>, Task>;
+    public class MyMiddleware
+    {
+        private readonly AppFun _env;
+
+        public MyMiddleware(AppFun env)
+        {
+            if (env == null) throw new ArgumentNullException("OWIN环境参数为空");
+            this._env = env;
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            var responseBody = "Linux.NET 学习手记（8）&nbsp;        --小蝶惊鸿";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseBody);
+            ((Stream)env["owin.ResponseBody"]).Write(responseBodyBytes, 0, responseBodyBytes.Length);
+            ((IDictionary<string, string[]>)env["owin.ResponseHeaders"]).Add("Content-Type", new string[] { "text/html; charset=utf-8" });
+            return this._env(env);
+        }
+    }
+}
---
+++ Linux_NET_8/Demo1/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo1
+{
+    using Owin;
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(typeof(MyMiddleware));
+            
+        }
+    }
+}
